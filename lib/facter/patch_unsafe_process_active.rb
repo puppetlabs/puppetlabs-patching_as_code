54dc59283130e9708d0e01f62dddc04e9c40da59
kreeuwijk <kevin.reeuwijk@puppet.com>
2020-12-15 17:21:01 +0100

    rubocop

--- lib/facter/patch_unsafe_process_active.rb
+++ lib/facter/patch_unsafe_process_active.rb
@@ -12,13 +12,13 @@ Facter.add('patch_unsafe_process_active') do
       end
       true if tasklist.include? processname.downcase
     end
-    
+
     processfile = Pathname.new(Puppet.settings['confdir'] + '/patching_unsafe_processes')
     result = false
     if processfile.exist?
       unsafe_processes = File.open(processfile, 'r').read
       unsafe_processes.each_line do |line|
-        next if line =~ /^#|^$/
+        next if line =~ %r{^#|^$}
         next if process_running(line.chomp) == false
         result = true
         break
