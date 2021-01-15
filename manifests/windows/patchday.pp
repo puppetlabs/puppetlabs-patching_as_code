e4ffaf91f4984f08b6394c70cdfd6b05751bc445
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2021-01-15 11:43:01 +0100

    Fix risk of duplicate declarations (#11)
    * Linux patching using collectors
    
    * align structure of Windows patchday code
    
    * bump version, update changelog

--- manifests/windows/patchday.pp
+++ manifests/windows/patchday.pp
@@ -2,29 +2,23 @@
 # Performs the actual patching on Windows
 #
 class patching_as_code::windows::patchday (
-  Array $updates,
-  String $patch_fact,
+  Array   $updates,
+  String  $patch_fact,
   Boolean $reboot
 ) {
 
+  $fact_refresh = Exec["${patch_fact}::exec::fact"]
+  $patch_reboot = Reboot['Patching as Code - Patch Reboot']
+
   $updates.each | $kb | {
-    if $reboot {
-      windows_updates::kb { $kb:
-        ensure      => 'present',
-        maintwindow => 'Patching as Code - Patch Window',
-        notify      => [
-          Exec["${patch_fact}::exec::fact"],
-          Reboot['Patching as Code - Patch Reboot']
-        ]
-      }
-    } else {
-      windows_updates::kb { $kb:
-        ensure      => 'present',
-        maintwindow => 'Patching as Code - Patch Window',
-        notify      => [
-          Exec["${patch_fact}::exec::fact"],
-        ]
-      }
+    $triggers = $reboot ? {
+      true  => [ $fact_refresh, $patch_reboot ],
+      false => [ $fact_refresh ]
+    }
+    windows_updates::kb { $kb:
+      ensure      => 'present',
+      maintwindow => 'Patching as Code - Patch Window',
+      notify      => $triggers
     }
   }
 }
