e9bcd1df9af23ab48b805a5e44a483a6a18147db
kreeuwijk <kevin.reeuwijk@puppet.com>
2021-01-20 14:42:01 +0100

    agent-side resource assertion

---
+++ lib/puppet/type/patch_package.rb
@@ -0,0 +1,111 @@
+# rubocop:disable Style/FrozenStringLiteralComment
+# rubocop:enable Style/FrozenStringLiteralComment
+Puppet::Type.newtype(:patch_package) do
+  @doc = 'Define a package resource to patch'
+
+  newparam(:name) do
+    isnamevar
+    desc 'Name of the package to patch'
+  end
+
+  newparam(:patch_window) do
+    desc 'Puppet schedule to link package resource to'
+  end
+
+  newparam(:cache_clean, array_matching: :all) do
+    desc 'Name of Exec resource to clean cache'
+
+    munge do |values|
+      values = [values] unless values.is_a? Array
+      values
+    end
+  end
+
+  newparam(:triggers, array_matching: :all) do
+    desc 'Resources to notify after updating the package'
+
+    munge do |values|
+      values = [values] unless values.is_a? Array
+      values
+    end
+  end
+
+  # All parameters are required
+  validate do
+    [:name, :patch_window, :cache_clean, :triggers].each do |param|
+      raise Puppet::Error, "Required parameter missing: #{param}" unless @parameters[param]
+    end
+  end
+
+  # See if the package to patch exists in the catalog
+  # If package is found, update resource one-time for patching
+  # If package is not found, create a one-time package resource
+  def pre_run_check
+    # Validate :cache_clean
+    cache_clean = parameter(:cache_clean)
+    cache_clean.value.each do |res|
+      retrieve_resource_reference(res)
+    rescue ArgumentError => e
+      raise Puppet::Error, "Parameter cache_clean failed: #{e} at #{@file}:#{@line}"
+    end
+
+    # Validate :triggers
+    triggers = parameter(:triggers)
+    triggers.value.each do |res|
+      retrieve_resource_reference(res)
+    rescue ArgumentError => e
+      raise Puppet::Error, "Parameter triggers failed: #{e} at #{@file}:#{@line}"
+    end
+
+    package = self[:name]
+    package_res = "Package[#{package}]"
+    begin
+      res = retrieve_resource_reference(package_res)
+      package_in_catalog = true
+    rescue ArgumentError
+      package_in_catalog = false
+    end
+
+    if package_in_catalog
+      if ['present', 'installed', 'latest'].include?(res['ensure'].to_s)
+        Puppet.send('notice', "#{package_res} (managed) will be updated as part of patching_as_code")
+        catalog.resource(package_res)['ensure'] = 'latest'
+        catalog.resource(package_res)['schedule'] = self[:patch_window]
+        catalog.resource(package_res)['require'] = Array(catalog.resource(package_res)['require']) + cache_clean.value
+        catalog.resource(package_res)['notify'] = Array(catalog.resource(package_res)['notify']) + triggers.value
+      else
+        Puppet.send('notice', "#{package_res} (managed) will not be updated as part of patching_as_code (ensure => <version> detected)")
+      end
+    else
+      Puppet.send('notice', "#{package_res} (unmanaged) will be updated as part of patching_as_code")
+      catalog.add_resource(Puppet::Type.type('package').new(
+                             title: package,
+                             ensure: 'latest',
+                             schedule: self[:patch_window],
+                             require: cache_clean.value,
+                             notify: triggers.value,
+                           ))
+    end
+  end
+
+  def retrieve_resource_reference(res)
+    case res
+    when Puppet::Type      # rubocop:disable Lint/EmptyWhen
+    when Puppet::Resource  # rubocop:disable Lint/EmptyWhen
+    when String
+      begin
+        Puppet::Resource.new(res)
+      rescue ArgumentError
+        raise ArgumentError, "#{res} is not a valid resource reference"
+      end
+    else
+      raise ArgumentError, "#{res} is not a valid resource reference"
+    end
+
+    resource = catalog.resource(res.to_s)
+
+    raise ArgumentError, "#{res} is not in the catalog" unless resource
+
+    resource
+  end
+end
