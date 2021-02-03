f72a2c6954534683b9e5c67ca48cb08357c53604
kreeuwijk <kevin.reeuwijk@puppet.com>
2021-02-03 14:05:50 +0100

    test

--- lib/puppet/type/reboot_if_pending.rb
+++ lib/puppet/type/reboot_if_pending.rb
@@ -63,6 +63,7 @@ Puppet::Type.newtype(:reboot_if_pending) do
     pre_reboot_resources = []
     catalog.resources.each do |res|
       puts "pre_pre_filtered resource: #{res}"
+      puts "pre_pre_filtered resource type: #{res.type}"
       next unless res['tag'].is_a? Array
       puts "pre_filtered resource: #{res}"
       next unless res['tag'] & ['patching_as_code_pre_patching', 'patching_as_code_post_patching', 'patching_as_code_pre_reboot'].any?
