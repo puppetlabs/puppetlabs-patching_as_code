35938b7e46f8eb5a46d4b660d1060bff3a60f99c
kreeuwijk <kevin.reeuwijk@puppet.com>
2022-02-14 14:51:14 +0100

    properly handle package subcomponents

--- lib/facter/patching_as_code_choco.rb
+++ lib/facter/patching_as_code_choco.rb
@@ -14,13 +14,16 @@ Facter.add('patching_as_code_choco') do
     # Determine Pinned packages (to be excluded from updating)
     output.each do |line|
       data = line.split('|')
+      next if pinned.include? data[0].split('.')[0]
+
       pinned.push(data[0]) if data[3] == 'true'
     end
     # Determine packages to update
     output.each do |line|
       data = line.split('|')
-      # Exclude subcomponents of pinned packages
+      # Exclude subcomponents of packages
       next if pinned.include? data[0].split('.')[0]
+      next if packages.include? data[0].split('.')[0]
 
       packages.push(data[0]) if data[3] == 'false'
     end
