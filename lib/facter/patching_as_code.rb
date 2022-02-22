e012064de6bafa1978222e3f0468a87545fa4f8c
kreeuwijk <kevin.reeuwijk@puppet.com>
2022-02-22 17:50:29 +0100

    change logic

--- lib/facter/patching_as_code.rb
+++ lib/facter/patching_as_code.rb
@@ -1,9 +1,16 @@
 
 Facter.add('patching_as_code') do
   setcode do
-    last_run_file = "#{Facter.value(:puppet_vardir)}/../../${patch_fact}/patching_as_code_last_run"
-    if File.exist?(last_run_file)
-      last_run = File.read(last_run_file)
+    option1 = "#{Facter.value(:puppet_vardir)}/../../pe_patch/patching_as_code_last_run"
+    option2 = "#{Facter.value(:puppet_vardir)}/../../os_patching/patching_as_code_last_run"
+    if File.exist?(option1)
+      last_run = File.read(option1)
+      result = {}
+      result['last_run'] = last_run
+      result['days_since_last_run'] = 'Date.parse(last_run) - Date.now'
+      result
+    elsif File.exist?(option2)
+      last_run = File.read(option2)
       result = {}
       result['last_run'] = last_run
       result['days_since_last_run'] = 'Date.parse(last_run) - Date.now'
