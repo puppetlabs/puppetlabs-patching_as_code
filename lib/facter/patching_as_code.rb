91d0e8a4b58d707434ff87bc82eb24fb55247f22
kreeuwijk <kevin.reeuwijk@puppet.com>
2022-03-07 12:25:29 +0100

    fix logic

--- lib/facter/patching_as_code.rb
+++ lib/facter/patching_as_code.rb
@@ -13,35 +13,39 @@ Facter.add('patching_as_code') do
       last_run           = JSON.parse(File.read(file)) if File.exist?(file)
       high_prio_last_run = JSON.parse(File.read(high_prio_file)) if File.exist?(high_prio_file)
       result = {}
-      if defined?(last_run)
-        result['last_patch_run']                        = last_run['last_run']
-        result['days_since_last_patch_run']             = (DateTime.now.to_date - DateTime.strptime(last_run['last_run'], '%Y-%m-%d %H:%M').to_date).to_i
-        result['patches_installed_on_last_run']         = last_run['patches_installed']
-        result['choco_patches_installed_on_last_run']   = last_run['choco_patches_installed']
-      else
+      if last_run.nil?
         result['last_patch_run']                        = ''
         result['days_since_last_patch_run']             = 0
         result['patches_installed_on_last_run']         = []
         result['choco_patches_installed_on_last_run']   = []
-      end
-      if defined?(high_prio_last_run)
-        result['last_high_prio_patch_run']                      = high_prio_last_run['last_run']
-        result['days_since_last_high_prio_patch_run']           = (DateTime.now.to_date - DateTime.strptime(high_prio_last_run['last_run'], '%Y-%m-%d %H:%M').to_date).to_i
-        result['patches_installed_on_last_high_prio_run']       = high_prio_last_run['patches_installed']
-        result['choco_patches_installed_on_last_high_prio_run'] = high_prio_last_run['choco_patches_installed']
       else
+        result['last_patch_run']                        = last_run['last_run']
+        result['days_since_last_patch_run']             = (DateTime.now.to_date - DateTime.strptime(last_run['last_run'], '%Y-%m-%d %H:%M').to_date).to_i
+        result['patches_installed_on_last_run']         = last_run['patches_installed']
+        result['choco_patches_installed_on_last_run']   = last_run['choco_patches_installed']
+      end
+      if high_prio_last_run.nil?
         result['last_high_prio_patch_run']                      = ''
         result['days_since_last_high_prio_patch_run']           = 0
         result['patches_installed_on_last_high_prio_run']       = []
         result['choco_patches_installed_on_last_high_prio_run'] = []
+      else
+        result['last_high_prio_patch_run']                      = high_prio_last_run['last_run']
+        result['days_since_last_high_prio_patch_run']           = (DateTime.now.to_date - DateTime.strptime(high_prio_last_run['last_run'], '%Y-%m-%d %H:%M').to_date).to_i
+        result['patches_installed_on_last_high_prio_run']       = high_prio_last_run['patches_installed']
+        result['choco_patches_installed_on_last_high_prio_run'] = high_prio_last_run['choco_patches_installed']
       end
       result
     rescue
       {
-        'last_patch_run' => '',
-        'days_since_last_patch_run' => 0,
-        'patches_installed_on_last_run' => [],
-        'choco_patches_installed_on_last_run' => []
+        'last_patch_run'                                => '',
+        'days_since_last_patch_run'                     => 0,
+        'patches_installed_on_last_run'                 => [],
+        'choco_patches_installed_on_last_run'           => [],
+        'last_high_prio_patch_run'                      => '',
+        'days_since_last_high_prio_patch_run'           => 0,
+        'patches_installed_on_last_high_prio_run'       => [],
+        'choco_patches_installed_on_last_high_prio_run' => []
       }
     end
   end
