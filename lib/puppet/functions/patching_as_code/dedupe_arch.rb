a1fa506958eeda7b88f9a2c7b8aea622ad200663
Kevin Reeuwijk <kevin.reeuwijk@spectrocloud.com>
2022-06-20 13:12:27 +0200

    linting

--- lib/puppet/functions/patching_as_code/dedupe_arch.rb
+++ lib/puppet/functions/patching_as_code/dedupe_arch.rb
@@ -4,14 +4,16 @@ Puppet::Functions.create_function(:'patching_as_code::dedupe_arch') do
   end
 
   def dedupe_arch(patches)
-    no_arch = patches.map { |patch| 
-      patch.sub(/(.noarch|.x86_64|.i386|.i686)$/, '')
-    }
-    multi_arch = no_arch.group_by{|x| x }.select { |k, v| v.size > 1 }.map(&:first)
-    result = patches.map { |patch|
-      no_arch_patch = patch.sub(/(.noarch|.x86_64|.i386|.i686)$/, '')
-      if multi_arch.include? no_arch_patch then no_arch_patch else patch end
-    }
+    no_arch = patches.map { |patch| patch.sub(%r{(.noarch|.x86_64|.i386|.i686)$}, '') }
+    multi_arch = no_arch.group_by { |x| x }.select { |_k, v| v.size > 1 }.map(&:first)
+    result = patches.map do |patch|
+      no_arch_patch = patch.sub(%r{(.noarch|.x86_64|.i386|.i686)$}, '')
+      if multi_arch.include? no_arch_patch
+        no_arch_patch
+      else
+        patch
+      end
+    end
     result.uniq
   end
 end
