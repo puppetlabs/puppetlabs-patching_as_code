d455e024feefe8db67e9d58ec599ba940c1f4f94
Kevin Reeuwijk <kevin.reeuwijk@spectrocloud.com>
2022-07-15 15:45:32 +0200

    add support for unsafe process arguments

--- lib/facter/patch_unsafe_process_active.rb
+++ lib/facter/patch_unsafe_process_active.rb
@@ -3,12 +3,23 @@ require 'pathname'
 Facter.add('patch_unsafe_process_active') do
   confine { Facter.value(:kernel) == 'windows' || Facter.value(:kernel) == 'Linux' }
   setcode do
-    def process_running(processname)
-      case Facter.value(:kernel)
+    def process_running(processname, full = false)
+      kernel = 'Linux'
+      case kernel
       when 'windows'
-        tasklist = `tasklist`.downcase
+        if full
+          tasklist = `wmic path win32_process get Commandline`.downcase
+          processname = processname[6..-1].strip
+        else
+          tasklist = `wmic path win32_process get Caption`.downcase
+        end
       when 'Linux'
-        tasklist = `ps -A`.downcase
+        if full
+          tasklist = `ps -Ao cmd`.downcase
+          processname = processname[6..-1].strip
+        else
+          tasklist = `ps -Ao comm`.downcase
+        end
       end
       tasklist.include? processname.downcase
     end
@@ -19,11 +30,15 @@ Facter.add('patch_unsafe_process_active') do
       unsafe_processes = File.open(processfile, 'r').read
       unsafe_processes.each_line do |line|
         next if line.match?(%r{^#|^$})
-        next if process_running(line.chomp) == false
+        if line.match?(%r{^{full}})
+          next if process_running(line.chomp, true) == false
+        elsif process_running(line.chomp) == false
+          next
+        end
         result = true
         break
       end
     end
-    result
+    puts result
   end
 end
