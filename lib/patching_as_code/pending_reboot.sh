bdd3e0f6ae94e104446f442f1692a74105c7449d
Kevin Reeuwijk <kevin.reeuwijk@spectrocloud.com>
2022-10-26 10:42:47 +0200

    use full facter path

--- lib/patching_as_code/pending_reboot.sh
+++ lib/patching_as_code/pending_reboot.sh
@@ -3,7 +3,7 @@ NEED_REBOOT=0
 
 if [ -f '/usr/bin/needs-restarting' ]
 then
-  case $(facter os.release.major) in
+  case $(/opt/puppetlabs/bin/facter os.release.major) in
     7|8|9)
       /usr/bin/needs-restarting -r 2>/dev/null 1>/dev/null
       if [ $? -eq 1 ]
@@ -21,7 +21,7 @@ then
   esac
 fi
 
-if [ $(facter osfamily) = 'Debian' ] || [ $(facter osfamily) = 'Suse' ]
+if [ $(/opt/puppetlabs/bin/facter osfamily) = 'Debian' ] || [ $(/opt/puppetlabs/bin/facter osfamily) = 'Suse' ]
 then
   if [ -f '/var/run/reboot-required' ]
   then
@@ -30,4 +30,4 @@ then
 fi
 
 # flip result for correct exitcode (0 means reboot needed, 1 means no reboot needed)
-[[ $NEED_REBOOT == 1 ]]
+[[ $NEED_REBOOT -eq 1 ]]
