fa2a209d13ebc0ea9f73a370b67a02b9c89bac15
Kevin Reeuwijk <kevin.reeuwijk@spectrocloud.com>
2022-10-26 15:39:20 +0200

    update check

--- lib/patching_as_code/pending_reboot.ps1
+++ lib/patching_as_code/pending_reboot.ps1
@@ -1,20 +1,24 @@
-#Copied from http://ilovepowershell.com/2015/09/10/how-to-check-if-a-server-needs-a-reboot/
-#Adapted from https://gist.github.com/altrive/5329377
-#Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
+function Get-PendingReboot {
+    #Copied from http://ilovepowershell.com/2015/09/10/how-to-check-if-a-server-needs-a-reboot/
+    #Adapted from https://gist.github.com/altrive/5329377
+    #Based on <http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542>
 
-$rebootPending = $false
+    $rebootPending = $false
 
-if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { $rebootPending = $true }
-if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { $rebootPending = $true }
-if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { $rebootPending = $true }
-try {
-    $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
-    $status = $util.DetermineIfRebootPending()
-    if (($null -ne $status) -and $status.RebootPending) {
-        $rebootPending = $true
+    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { $rebootPending = $true }
+    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { $rebootPending = $true }
+    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { $rebootPending = $true }
+    try {
+        $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+        $status = $util.DetermineIfRebootPending()
+        if (($null -ne $status) -and $status.RebootPending) {
+            $rebootPending = $true
+        }
     }
-}
-catch { }
+    catch { }
 
-# return result based on exitcode (0 means reboot needed, 1 means no reboot needed)
-if ($rebootPending) { exit 0 } else { exit 1 } 
+    # return result
+    $rebootPending
+}
+
+Get-PendingReboot
